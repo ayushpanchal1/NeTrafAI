bdec5d82cd9edd4a9f1abd492e7c8620dd788322
pandyadhiraj <115932767+pandyadhiraj@users.noreply.github.com>
2024-11-10 23:22:34 +0530

    Delete ayush directory

--- ayush/captureNimage/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-            # plt.subplot(121)  # 1 row, 2 columns, 1st subplot
-            # plt.pie(self.packet_counts.values(), labels=self.packet_counts.keys(), autopct='%1.1f%%', startangle=90)
-            # plt.title('Packet Protocol Distribution')
-            # plt.tight_layout()
